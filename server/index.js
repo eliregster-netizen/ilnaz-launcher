decb7ef36f52a73153e3074240b7d4f58a8d2933
eliregster <eliregster@gmail.com>
2026-05-02 10:25:02 +0300

    fix: replace bcrypt with bcryptjs to fix Render native module error

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { MongoClient } = require('mongodb');
 
